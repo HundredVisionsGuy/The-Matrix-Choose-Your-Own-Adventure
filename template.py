229b38a6598f944d14d7d7392a80cb9ec4ee72c6
hundredvisionsguy <winikkc@hsd.k12.or.us>
2015-03-05 14:57:29 -0800

    Added template.py

---
+++ template.py
@@ -0,0 +1,59 @@
+# Choose.py
+# by [YourNameHere]
+# Description: starter code for the Choose Your
+# Own Adventure Project
+
+# Import Statements
+from tkinter import *
+import tkinter.simpledialog
+import tkinter.messagebox
+
+root = Tk()
+w = Label(root, text="Choose Your Own Adventure")
+w.pack()
+
+def intro():
+    """ Introductory Function -> starts the story going """
+    messagebox.showinfo("Title", "\nHello, you are a _____ from ____. " + \
+                        "You stumble across such and such.")
+    choice = simpledialog.askinteger("Choose wisely",
+                                   "You have a choice to pick: 1 or 2.")
+    if choice == 1:
+        choice1()
+    elif choice == 2:
+        choice2()
+    else:
+        intro()
+
+################ Student A Functions #####################
+def choice1():
+    choice = simpledialog.askinteger("Choose wisely",
+                                     "This is the next part of the story.  Now you must choose 1 or 2 again.")
+    if (choice == 1):
+        messagebox.showinfo("The End",
+                            "You chose right.  THE END")
+
+    elif (choice == 2):
+        messagebox.showinfo("The End",
+                            "You chose ok.  THE END")
+    else:
+        choice1()
+
+################ Student B Functions #####################
+def choice2():
+    choice = simpledialog.askinteger("Choose wisely",
+                                     "This is the next part of the story.  Now you must choose 1 or 2 again.")
+    if (choice == 1):
+        messagebox.showinfo("The End",
+                            "You chose right.  THE END")
+
+    elif (choice == 2):
+        messagebox.showinfo("The End",
+                            "You chose ok.  THE END")
+    else:
+        choice2()
+
+################ Main #####################
+intro()
+
+root.destroy()
